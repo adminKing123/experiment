32ba4a6b2aebf1a77e6dea855c9f3f9aecfd139e
harsh <harshverma790932611@gmail.com>
2023-03-05 13:29:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+function Header() {
+  console.log("header");
+  return <h2>Header</h2>
+}
+
+function Footer() {
+  console.log("footer");
+  return <h2>Footer</h2>
+}
+
+function Main() {
+  const [index, setIndex] = useState(0);
+  const colors = ["black", "green", "red", "orange", "purple", "violet", "yellow", "blue", "pink"];
+  console.log("main");
+
+  return (
+    <div style={{
+      cursor: "pointer"
+    }}>
+      <h2 style={{
+        color: colors[index],
+      }} onClick={() => {
+        setIndex(prev => (prev + 1) % colors.length);
+      }}>Hello World</h2>
+      <p>{colors[index]}</p>
+    </div>
+  );
+}
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Main/>
+      <Footer />
+    </>
   );
 }
 
